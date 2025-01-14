9534582309f052ed456d3a03659950471bca57b2
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2025-01-14 18:46:39 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,58 +1,30 @@
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        //interface
         JFrame userFrame = new JFrame("Hotel Something");
-        userFrame.setSize(1200, 700);
+        userFrame.setSize(800, 600);
         userFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         userFrame.setLayout(null);
         userFrame.setVisible(true);
 
+        JTextField textDisplay = new JTextField();
+        textDisplay.setBounds(50, 50, 50, 50);
+        textDisplay.setEditable(false);
+        textDisplay.setFocusable(false);
+        userFrame.add(textDisplay);
+
         JButton startButton = new JButton("Start");
         startButton.setBounds(500, 300, 100, 50);
-        userFrame.add(startButton);
 
         startButton.addActionListener(new ActionListener() {
-            public void startButtonPressed(ActionEvent e) {
-                System.out.println("Start was pressed");
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textDisplay.setText("Start");
             }
         });
-        
-        
-        JTextField textInput = new JTextField();
-        textInput.setBounds(100, 100, 400, 50);
-        //userWindow.add(textInput);
 
-        
-        JLabel label = new JLabel();
-        label.setText("aaaaaaaaaaaaaaaaaaaaaaa");
-        JPanel panel = new JPanel();
-        
-        panel.add(label);
-        userWindow.add(panel);
-        userWindow.show();
-
-      
-        /*
-        Scanner userInput = new Scanner(System.in);
-        
-        //storage
-        String rooms[] = new String[10];
-        String reserveRequest = null;
-        
-        System.out.println("question");
-        reserveRequest = userInput.nextLine();
-        
-        int roomSelection = 0;
-        
-        if (rooms[roomSelection].isEmpty()) {
-            rooms[roomSelection] = reserveRequest;
-            System.out.println("result a");
-            
-        } else {
-            System.out.println("result b");
-        }
-        */
+        userFrame.add(startButton);
     }
 }
