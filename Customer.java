f4605deec16f7ee44c8ef0c4dd5f3862688e085b
roxashrb <hbr0357@students.uc-bcf.edu.ph>
2025-03-06 20:10:32 +0800

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,24 @@
+package hotelreservationsystem;
+
+public class Customer {
+    private String name;
+    private int phoneNumber;
+    private String address;
+    private int roomNumber;
+    
+    public void checkIn() {
+        
+    }
+    
+    public void checkOut() {
+        
+    }
+    
+    public void orderFoodItem() {
+        
+    }
+    
+    public void payBill() {
+        
+    }
+}
