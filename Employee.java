8d144a2771fc9a45f0ed3a25c03899ebf869c40c
roxashrb <hbr0357@students.uc-bcf.edu.ph>
2025-03-06 20:02:09 +0800

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,1 +1,40 @@
+package hotelreservationsystem;
 
+public class Employee {
+    private String name;
+    private int id;
+    private int phoneNumber;
+    private String address;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
