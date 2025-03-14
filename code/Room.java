1b80c1343aacae235be696415182774413dc1c59
roxashrb <hbr0357@students.uc-bcf.edu.ph>
2025-03-14 17:28:49 +0800

    Update Room.java

--- code/Room.java
+++ code/Room.java
@@ -1,4 +1,17 @@
 public class Room {
     private int roomNumber;
     private boolean reservedStatus;
+
+    public int getRoomNumber() {
+    }
+
+    public void setRoomNumber(int roomNumber) {
+    }
+
+    public boolean getReservedStatus() {
+    }
+
+    public void setReservedStatus(boolean reservedStatus) {
+        this.reservedStatus = reservedStatus;
+    }
 }
