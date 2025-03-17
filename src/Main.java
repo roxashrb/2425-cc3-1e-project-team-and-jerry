b6abb2fd20e54ddb9e7e6fa078afe96d457e82ef
roxashrb <hbr0357@students.uc-bcf.edu.ph>
2025-03-17 18:05:50 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DataHelper readWrite = new DataHelper();
         Scanner userInput = new Scanner(System.in);
         System.out.println("Are you a customer or an employee?");
     }
