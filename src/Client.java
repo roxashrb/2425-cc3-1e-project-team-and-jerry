4bf56bbd86b6581a78b384333c3b42657e898e75
roxashrb <hbr0357@students.uc-bcf.edu.ph>
2025-03-19 17:31:17 +0800

    Create Client.java

---
+++ src/Client.java
@@ -0,0 +1,49 @@
+public class Client {
+	private String clientType;
+	private int userId;
+	private String userName;
+	private String userToken;
+	private boolean loginStatus;
+
+	public void setClientType(String clientType) {
+		this.clientType = clientType;
+	}
+
+	public String getClientType() {
+		return clientType;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserToken(String userToken) {
+		this.userToken = userToken;
+	}
+
+	public void setLoginStatus(boolean loginStatus) {
+		this.loginStatus - loginStatus);
+	}
+
+	public boolean getLoginStatus() {
+		return loginStatus;
+	}
+
+	public void login() {
+	}
+
+	public void logout() {
+	}
+}
