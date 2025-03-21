4191694d4cf8caf3e0d792666b348be9f8146cde
roxashrb <hbr0357@students.uc-bcf.edu.ph>
2025-03-21 17:21:47 +0800

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,49 +1,20 @@
 public class Client {
 	private String clientType;
-	private int userId;
-	private String userName;
-	private String userToken;
-	private boolean loginStatus;
-
-	public void setClientType(String clientType) {
-		this.clientType = clientType;
-	}
+	private String clientName;
 
 	public String getClientType() {
 		return clientType;
 	}
 
-	public void setUserId(int userId) {
-		this.userId = userId;
+	public void setClientType(String clientType) {
+		this.clientType = clientType;
 	}
 
-	public int getUserId() {
-		return userId;
+	public String getClientName() {
+		return clientName;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserToken(String userToken) {
-		this.userToken = userToken;
-	}
-
-	public void setLoginStatus(boolean loginStatus) {
-		this.loginStatus - loginStatus);
-	}
-
-	public boolean getLoginStatus() {
-		return loginStatus;
-	}
-
-	public void login() {
-	}
-
-	public void logout() {
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
 	}
 }
