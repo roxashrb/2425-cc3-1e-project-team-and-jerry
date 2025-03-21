07d6eee53d13709542eeacf92ad8a958c1d549b4
roxashrb <hbr0357@students.uc-bcf.edu.ph>
2025-03-21 17:57:15 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Are you a customer or an employee?");
+        boolean active = true;
+        while (boolean) {
+            System.out.println("set client type: \n1. Manager\n2.Customer");
+            int clientType = userInput.nextInt();
+            switch (clientType) {
+                case 1:
+                    Manager managerClient = new Manager();
+                    setClientType("Manager");
+                    break;
+                case 2:
+                    Customer customerClient = new Customer();
+                    setClientType("Customer");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }
     }
 }
