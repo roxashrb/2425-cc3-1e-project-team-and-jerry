98d4a3ab1a02f7bc70d6b4f6c92d0402d04ca2a3
roxashrb <hbr0357@students.uc-bcf.edu.ph>
2025-03-31 17:51:02 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,12 @@ public class Main {
         boolean active = true;
         String clientName;
         String clientType;
+        
         while (active) {
             System.out.println("set client type: \n1. Manager\n2.Customer");
             int clientTypeSelected = userInput.nextInt();
             userInput.nextLine();
+            
             switch (clientType) {
                 case 1:
                     Manager managerClient = new Manager();
@@ -21,6 +23,7 @@ public class Main {
                     clientName = userInput.nextLine();
                     managerClient.setClientName(clientName);
                     break;
+                    
                 case 2:
                     Customer customerClient = new Customer();
                     customerClient.setClientType("Customer");
@@ -29,27 +32,51 @@ public class Main {
                     clientName = userInput.nextLine();
                     customerClient.setClientName(clientName);
                     break;
+                    
                 default:
                     System.out.println("Invalid choice");
                     break;
             }
 
-            System.out.println("Choose operation:\n1. Create a reservation");
-            int operation = userInput.nextInt();
-            long timestamp = System.currentTimeMillis();
-            switch (operation) {
-                case 1:
-                    try {
-                        File reservation = new File(clientType + "-" + timestamp);
-                        if (reservation.createNewFile()) {
-                            System.out.println("File created: " + reservation.getName());
-                        } else {
-                            System.out.println("File already exists.");
+            if (clientType.equals("Manager")) {
+                System.out.println("Choose operation:\n1.Read a reservation");
+                int task = userInput.nextInt();
+                userInput.nextLine();
+                System.out.println("Input the name of the reservation to read: ");
+                String filenameToRead = userInput.nextLine();
+
+                switch (task) {
+                    case 1:
+                        try {
+                            File reservationToRead = new File(filenameToRead);
+                            
+                            while (userInput.hasNextLine()) {
+                                String data = userInput.nextLine();
+                                System.out.println(data);
+                            }
+                        } catch (FileNotFoundException e) {
+                            System.out.println("An error occurred.");
+                            e.printStackTrace();
                         }
-                    } catch (IOException e) {
-                        System.out.println("An error occurred.");
-                        e.printStackTrace();
-                    }
+                }
+            } else {
+                System.out.println("Choose task:\n1:Create a reservation");
+                int task = userInput.nextInt();
+                long timestamp = System.currentTimeMillis();
+                
+                switch (task) {
+                    case 1:
+                        try {
+                            File reservation = new File(clientType + "-" + timestamp);
+                            if (reservation.createNewFile()) {
+                                System.out.println("File created: " + reservation.getName());
+                            } else {
+                                System.out.println("File already exists.");
+                            }
+                        } catch (IOException e) {
+                            System.out.println("An error occurred.");
+                            e.printStackTrace();
+                        }
                     break;
             }
         }
